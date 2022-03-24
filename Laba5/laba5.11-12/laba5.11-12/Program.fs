69622a6960d293de2b43a174971ba6867fba07db
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-03-25 00:28:04 +0300

    5 лаба, 11 задание

---
+++ Laba5/laba5.11-12/laba5.11-12/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let Check s =
+    if (s="F#"||s="Prolog") then "Вы подлиза!"
+    else "Спасибо за ответ!"
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Пожалуйста, введите ваш любимый язык программирования:")
+    let s=Console.ReadLine()
+    Console.WriteLine(Check s)
+    0
