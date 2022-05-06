38ff67ce6148dd4b70ba2367f504bb4a4b4636ac
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-05-06 13:08:40 +0300

    переделал 4

--- laba6/laba6.13 (4)/laba6.13 (4)/Program.fs
+++ laba6/laba6.13 (4)/laba6.13 (4)/Program.fs
@@ -19,46 +19,94 @@ let rec writeList = function
     | (head : int)::tail -> 
                        System.Console.WriteLine(head)
                        writeList tail  
-let findMax list=
-    let rec findm list max index maxindex=
+let elem list i =
+    let rec el list i count =
         match list with
-        |h::t-> 
-            let newindex=index+1
-            let newmax=if h>max then h else max
-            let newmaxindex=if h=newmax then newindex else maxindex
-            let newlist=t
-            findm newlist newmax newindex newmaxindex
-        |[]->maxindex
-    findm list list.Head 0 0
- 
-let DelElem list n =
-    let rec recDelElem list n curlist curIdx  =
-        match list with
+        |[]->0
         |h::t->
-            if n = curIdx then curlist @ t
+            if i=count then h
             else
-                let newCurlist = curlist @ [list.Head]
-                recDelElem t n newCurlist (curIdx+1)
-        |[]->[]
-    recDelElem list n [] 1
+                el t i (count+1)
+    el list i 1
 
-let ShowIndex list=
-    let rec recshoww list i maxindex newlist=
+let DelElem list n =
+    let rec DelElem (list:'a list) n (curlist:'a list) curIdx  =
+        if n = curIdx then curlist @ list.Tail
+        else
+            let newCurlist = curlist @ [list.Head]
+            DelElem list.Tail n newCurlist (curIdx+1)
+    DelElem list n [] 1
+
+let findIndex list x =
+    let rec FindIndex list x i =
         match list with
+        |[]-> -1
         |h::t->
-            if i=maxindex then 
-                        let newnewlist=newlist @ [i]
-                        let novlist=DelElem list i
-                        if novlist<>[] then recshoww novlist 1 (findMax novlist) newnewlist
-                        else newnewlist
-            else 
-                recshoww list (i+1) maxindex newlist
-        |_->newlist
-    recshoww list 1 (findMax list) []
+            if h=x then i 
+            else
+                FindIndex t x (i+1)
+    FindIndex list x 1
+
+
+let findMax list =
+    let rec Max list max =
+        match list with
+        |[]->max
+        |h::t->
+            let newCur = if (h>max) then h else max
+            let newList = if t<>[] then t else []
+            Max newList newCur
+    Max list list.Head
+
+let findMin list =
+    let rec rec_findMax list cur =
+        match list with
+        |[]->cur
+        |h::t->
+            let newCur = if (h<cur) then h else cur
+            let newList = if t<>[] then t else []
+            rec_findMax newList newCur
+    rec_findMax list list.Head
+
+
+let Have list x =
+    let rec have list x =
+        match list with
+        |[]-> false
+        |h::t->
+            if (h=x) then true else have t x
+    have list x
+
+
+let revSort list =
+    let rec recRevSort list curlist =
+        match list with
+        |[]->curlist
+        |_->
+            let max = findMax list
+            let idxMax = findIndex list max
+            let newList = DelElem list idxMax
+            let newCurlist = curlist @ [max]
+            recRevSort newList newCurlist
+    recRevSort list []
+
+
+let rec MakeList list min max curEl curList =
+    if curEl = (max+1) then curList
+    else 
+        if (Have list curEl && Have curList (findIndex list curEl) = false) then 
+            let idxEl = findIndex list curEl
+            let newCurlist = [idxEl] @ curList
+            
+            MakeList list min max curEl newCurlist
+        else
+            MakeList list min max (curEl+1) curList
+    
 
 [<EntryPoint>]
 let main argv =
-    let list=readData
-    Console.WriteLine(findMax list)
-    Console.WriteLine(ShowIndex list)
+    let list = readData
+    let ans = MakeList list (findMin list) (findMax list) (findMin list) []
+    Console.WriteLine("\n")
+    writeList ans
     0 // return an integer exit code
