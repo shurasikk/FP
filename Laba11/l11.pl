dcada088147043364eb05482c8524bded17964a9
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-05-17 00:45:21 +0300

    лаба 11 (12)

--- Laba11/l11.pl
+++ Laba11/l11.pl
@@ -2,11 +2,63 @@ man(ivan).
 man(alexandr).
 man(nikolay).
 man(aleksey).
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
 
-parent(ivan, vladimyr).
-parent(nikolay, viktoriya).
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
 
 %11 задание
 
 father(X,Y):-man(X),parent(X,Y),!.
 father(X):-father(X,Y),write(X),nl.
+
+%12 задание
+
+wife(X,Y):-woman(X),parent(X,Z),parent(Y,Z),nl.
+wife(X):-wife(X,Y), write(X), nl,!.
+
