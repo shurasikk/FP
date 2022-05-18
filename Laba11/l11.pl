33d9295893aff18a195d640b3fd3d41a54155d00
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-05-18 20:30:28 +0300

    лаба 11 (13)

--- Laba11/l11.pl
+++ Laba11/l11.pl
@@ -21,6 +21,7 @@ woman(bozhedara).
 woman(broneslava).
 woman(veselina).
 woman(zdislava).
+woman(galina).
 
 parent(voeneg,ratibor).
 parent(voeneg,bratislava).
@@ -31,6 +32,8 @@ parent(goluba,ratibor).
 parent(goluba,bratislava).
 parent(goluba,velerad).
 parent(goluba,zhdana).
+parent(galina,ivan).
+parent(ivan,nikolay).
 
 parent(ratibor,svyatoslav).
 parent(ratibor,dobrozhir).
@@ -52,13 +55,17 @@ parent(duhovlad,zlatomir).
 parent(zhdana,zdislava).
 parent(zhdana,zlatomir).
 
-%11 задание
+%11 zadaniye
 
 father(X,Y):-man(X),parent(X,Y),!.
 father(X):-father(X,Y),write(X),nl.
 
-%12 задание
-
+%12 zadaniye
 wife(X,Y):-woman(X),parent(X,Z),parent(Y,Z),nl.
 wife(X):-wife(X,Y), write(X), nl,!.
+
+%13 zadaniye
+grand_ma(X, Y):-woman(X),parent(X,Z),parent(Z,Y).
+grand_mas(X):-grand_ma(X,Y),write(X),nl.
+
 
