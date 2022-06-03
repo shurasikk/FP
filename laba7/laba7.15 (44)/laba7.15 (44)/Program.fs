b9da6f3c7767be6f7c5bda470b2e99ac67ec9adb
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-06-03 13:03:51 +0300

    задание 15 (44)

---
+++ laba7/laba7.15 (44)/laba7.15 (44)/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec writeList list= 
+    List.iter (printfn "%d" ) list
+
+let isInteger (x : float32) =
+    if x - (floor x) = 0.f then
+        1
+    else
+        0
+
+let checkList (list:float32 list) =
+    let intfloatlist = List.map isInteger list
+    let pairList = List.pairwise intfloatlist
+    let rec checking list=
+        match list with
+        |h::t->
+            if ((fst (h)=1 && snd(h)=0) || (fst(h)=1 && snd(h)=0)) then checking t
+            else false
+        |[]->true
+    let p=checking pairList
+    p
+
+[<EntryPoint>]
+let main argv =
+    let list = 
+        [1.f; 1.1f]
+    if checkList list then Console.WriteLine("чередуются")
+        else Console.WriteLine("не чередуются")
+    0 // return an integer exit code
