e1e306cb8aaf439754ccaf52adad09ce3bd13e4d
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-06-03 10:52:48 +0300

    задание 19 (метод 2)

--- laba7.19(4,11,15)/laba7.19(4,11,15)/Program.fs
+++ laba7.19(4,11,15)/laba7.19(4,11,15)/Program.fs
@@ -2,6 +2,19 @@
 
 open System
 
+let checkPalindrome (str:string) =
+    let charArray = str.ToCharArray()
+    let revCharArray = Array.Reverse charArray
+    let revString = (string)revCharArray
+    match str with
+    |_ when str=revString->true
+    |_->false
+
+let CountWords (str:string)=
+    let strnew=str.Split(" ")
+    let k=Array.length strnew
+    k
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
