256e3ff88223a173241a440b9b5b89b1c096bee8
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-06-03 12:39:24 +0300

    задание 20

--- laba7/laba7.20 (4,7)/laba7.20 (4,7)/Program.fs
+++ laba7/laba7.20 (4,7)/laba7.20 (4,7)/Program.fs
@@ -5,14 +5,15 @@ open System
 let getASCIICode x = 
     int x - int '0'
 
-let rec readString n strings=
-    match n with 
-    |0 -> strings
-    |_ -> 
-        let s = Console.ReadLine()
-        let newStrings = strings @ [s]
-        let n1 = n - 1
-        readString n1 newStrings
+let readString n =
+    let rec readStringRec n strings=
+        match n with 
+        |0 -> strings
+        |_ -> 
+            let s = Console.ReadLine()
+            let newStrings = strings @ [s]
+            readStringRec (n-1) newStrings
+    readStringRec n []
 
 let rec writeList list= 
     List.iter (printfn "%s" ) list
@@ -23,9 +24,8 @@ let AverageWeight string =
 
 let sortfirst (strings:'string list) =
     let firstStringWeight = AverageWeight strings.Head
-    Console.WriteLine strings.Head
     let sorted = List.sortBy (fun x-> pown ((AverageWeight x) - firstStringWeight) 2) strings.Tail
-    writeList (sorted)
+    sorted
 
 let isVowel (x : char) =
     (x = 'а') || (x = 'и') || (x = 'е') || (x = 'ё') || (x = 'о') || (x = 'у') || (x = 'ы') || (x = 'э') || (x = 'ю') || (x = 'я') || (x = 'u') || (x = 'i') || (x = 'o')
@@ -42,8 +42,24 @@ let countSoch str =
 let sortsecond (strings:'string list)=
     Seq.toList (Seq.sortBy countSoch strings)
 
+let choose k stringList =
+    match k with 
+    |1 -> sortfirst stringList
+    |2 -> sortsecond stringList
+    |_ -> exit(0)
 
 [<EntryPoint>]
 let main argv =
-    let stringList = readString 3 []
+    Console.WriteLine("Выберите задачу для решения и введите строку: \n Отсортировать строки в указанном порядке")
+    Console.WriteLine("1 В порядке увеличения квадратичного отклонения среднего веса ASCII-кода символа строки от среднего веса ASCII-кода символа первой строки")
+    Console.WriteLine("2 В порядке увеличения разницы между количеством сочетаний «гласная-согласная» и «согласная-гласная» в строке")
+    let k =
+        Convert.ToInt32(Console.ReadLine())
+    let n = 
+        Convert.ToInt32(Console.ReadLine())
+    let stringList = 
+        readString n
+    let newList =
+        choose k stringList
+    writeList newList
     0 // return an integer exit code
