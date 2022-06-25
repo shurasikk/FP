52212f448b66b67f0027ef7bdc231fae95cca40b
shurasikk <90204027+shurasikk@users.noreply.github.com>
2022-06-25 14:13:56 +0300

    задание 12 (46)

--- laba 13/laba13.pl
+++ laba 13/laba13.pl
@@ -15,3 +15,22 @@ first_ch([H|T],Out):-0 is H mod 2,Out is H;first_ch(T,Out),!.
 min([],M,M).
 min([H|T],M,Min):-(H<M,0 is H mod 2,M1 is H;M1 is M),min(T,M1,Min).
 min(List,Min):-first_ch(List,F),min(List,F,Min),!.
+
+
+%13 zadaniye (46)
+
+add([],[H|T],[H|T]):-!.
+add(E,[],[E]).
+add(E,[H|T],[H|T1]):-add(E,T,T1).
+
+listNegative(X,Res):-listNegative(X,Res,[]),!.
+listNegative([],Res,Res):-!.
+listNegative([H|T],Res,CurL):-(H<0,add(H,CurL,NewL);append([],CurL,NewL)),
+    listNegative(T,Res,NewL),!.
+
+listPositive(X,Res):-listPositive(X,Res,[]),!.
+listPositive([],Res,Res):-!.
+listPositive([H|T],Res,CurL):-(H>=0,add(H,CurL,NewL);add([],CurL,NewL)),
+    listPositive(T,Res,NewL),!.
+
+list_p_and_n(X,Res):- listPositive(X,Pos), listNegative(X,Neg), append(Pos,Neg,Res),!.
